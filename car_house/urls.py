3a1a73581233ce184b5c09c3b581c1716aef31c5
MD TALHA BIN ABDULLAH <jubairt35@gmail.com>
2023-12-15 12:52:49 +0600

    Details page add

--- car_house/urls.py
+++ car_house/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from . views import home
+from . views import home, DetailsCarView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('user/', include('user_validation.urls')),
     path('brand/<slug:brand_slug>', home, name='brand_slug'),
     path('', home, name='home'),
+    path('car_details/<int:id>', DetailsCarView.as_view(), name='car_details'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- car_house/views.py
+++ car_house/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from brand.models import Brand
 from car.models import Car
+from django.views.generic import DetailView
 
 def home(req, brand_slug = None) :
     data = Car.objects.all()
@@ -9,3 +10,10 @@ def home(req, brand_slug = None) :
         data = Car.objects.filter(brand=brd)
     brand = Brand.objects.all()
     return render(req, 'home.html', {'data' : data, 'brand' : brand})
+
+
+class DetailsCarView(DetailView) :
+    model = Car
+    pk_url_kwarg = 'id'
+    template_name = 'car_details.html'
+        
