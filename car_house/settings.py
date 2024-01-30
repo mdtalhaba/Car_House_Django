1901d1bc8a9b20cdd27ba0b873503d283ebfef2c
MD TALHA BIN ABDULLAH <jubairt35@gmail.com>
2024-01-30 23:00:06 +0600

    requirements add

--- car_house/settings.py
+++ car_house/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)k5nccqjm%t26p(@5ymhm6%#qi#58q$ofc#(pje(g-v-4rcgw#'
+SECRET_KEY = env('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
